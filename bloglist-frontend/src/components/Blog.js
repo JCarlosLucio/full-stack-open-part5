6a2c770f0aaa61cd027ff36cddc5ee6a59a178eb
jcLucio <c.lucio3@gmail.com>
2021-06-17 22:21:52 -0500

    Add bloglist frontend exercise (5.7) - Add view button for showing Blog details - Add blogStyle

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,35 @@
-import React from 'react';
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import React, { useState } from 'react';
 
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisibility = () => setVisible(!visible);
+
+  const details = { display: visible ? '' : 'none' };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={details}>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes}
+          <button>like</button>
+        </div>
+        <div>{blog.user.name}</div>
+      </div>
+    </div>
+  );
+};
 export default Blog;
