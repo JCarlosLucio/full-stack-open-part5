bc3561812bc005922e0547d2d1ca9dc1caaf4f52
jcLucio <c.lucio3@gmail.com>
2021-06-22 17:27:47 -0500

    Add bloglist end to end testing exercise (5.18) - Add Login tests for success and failure - Add id to login button

--- bloglist-frontend/cypress/integration/blog_app.spec.js
+++ bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    const user = {
+      username: 'yoshi',
+      name: 'Yoshi Tester',
+      password: '1234',
+    };
+    cy.request('POST', 'http://localhost:3003/api/users', user);
     cy.visit('http://localhost:3000');
   });
 
@@ -11,5 +17,26 @@ describe('Blog app', function () {
     cy.contains('password');
     cy.get('#password');
     cy.contains('login');
+  });
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('yoshi');
+      cy.get('#password').type('1234');
+      cy.get('#login-button').click();
+      cy.get('.notification.success')
+        .should('contain', 'Welcome back, Yoshi Tester!')
+        .and('have.css', 'color', 'rgb(0, 128, 0)');
+      cy.contains('Yoshi Tester logged in');
+    });
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('yoshi');
+      cy.get('#password').type('wrong');
+      cy.get('#login-button').click();
+      cy.get('.notification.error')
+        .should('contain', 'invalid username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)');
+    });
   });
 });
--- bloglist-frontend/src/components/LoginForm.js
+++ bloglist-frontend/src/components/LoginForm.js
@@ -30,7 +30,9 @@ const LoginForm = ({
           onChange={({ target }) => setPassword(target.value)}
         />
       </div>
-      <button type="submit">login</button>
+      <button id="login-button" type="submit">
+        login
+      </button>
     </form>
   );
 };
